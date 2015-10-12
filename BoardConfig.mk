89b0da92a9932bb296bc3dda059a5052c760bd54
Saatvik Shukla <me@saatvikshukla.com>
2015-10-12 11:48:10 +0530

    Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,206 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+LOCAL_PATH := device/sony/D2005
+
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+TARGET_NO_SEPARATE_RECOVERY := true
+BOARD_VENDOR := sony
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8610
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8610
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 maxcpus=2 msm_rtb.filter=0x3F ehci-hcd.park=3 msm_rtb.enable=0 lpj=192598 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y androidboot.selinux=permissive selinux=0
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/custombootimg.mk
+BOARD_KERNEL_BASE := 0x00008000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt kernel.elf.3
+# TARGET_KERNEL_SOURCE := kernel/sony/msm8610
+# TARGET_KERNEL_CONFIG := msm8610_build_defconfig
+
+#WLAN_MODULES:
+#	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+#	mv $(KERNEL_MODULES_OUT)/wlan.ko $(#KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+#	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $#(TARGET_OUT)/lib/modules/wlan.ko
+
+#TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# Audio
+AUDIO_FEATURE_DISABLED_ANC_HEADSET := true
+AUDIO_FEATURE_DISABLED_DS1_DOLBY_DDP := true
+AUDIO_FEATURE_DISABLED_SSR := true
+AUDIO_FEATURE_DEEP_BUFFER_PRIMARY := true
+AUDIO_FEATURE_DYNAMIC_VOLUME_MIXER := true
+BOARD_USES_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml 
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
+
+# Display
+BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_QCOM_DISPLAY_VARIANT := caf-new
+TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Media
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+TARGET_QCOM_MEDIA_VARIANT := caf-new
+
+# Qualcomm support
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2005
+
+# Init
+# TARGET_INIT_VENDOR_LIB := libinit_msm
+# TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_condor.c
+# TARGET_UNIFIED_DEVICE := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1310000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2020000000
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+# BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# SELinux
+# BOARD_SEPOLICY_DIRS += \
+#    $(LOCAL_PATH)/sepolicy
+#
+# BOARD_SEPOLICY_UNION += \
+#    adbd.te \
+#    app.te \
+#    bluetooth_loader.te \
+#    bridge.te \
+#    camera.te \
+#    device.te \
+#    dhcp.te \
+#    dnsmasq.te \
+#    domain.te \
+#    drmserver.te \
+#    file_contexts \
+#    file.te \
+#    hostapd.te \
+#    init_shell.te \
+#    init.te \
+#    libqc-opt.te \
+#    mediaserver.te \
+#    mpdecision.te \
+#    netd.te \
+#    netmgrd.te \
+#    nfc.te \
+#    property_contexts \
+#    property.te \
+#    qcom.te \
+#    qmux.te \
+#    radio.te \
+#    rild.te \
+#    rmt.te \
+#    sdcard_internal.te \
+#    sdcardd.te \
+#    sensors.te \
+#    shell.te \
+#    surfaceflinger.te \
+#    system.te \
+#    tee.te \
+#    te_macros \
+#    thermald.te \
+#    ueventd.te \
+#    vold.te \
+#    wpa_supplicant.te \
+#    zygote.te
+
+# ifneq ($(TARGET_BUILD_VARIANT),user)
+#    BOARD_SEPOLICY_UNION += su.te
+# endif
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Vold
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 40
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+TARGET_USES_WCNSS_CTRL := true
+
+# inherit from the proprietary version
+-include vendor/sony/D2005/BoardConfigVendor.mk
---
+++ cm.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/sony/D2005/full_D2005.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := Xperia E1
+PRODUCT_NAME := cm_D2005
